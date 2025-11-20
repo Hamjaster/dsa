465cdb1365f9d937a4ff0fef26b6cdfc3a46e329
Hamza <hamzasepal@gmail.com>
2025-11-20 21:22:02 +0500

    binary expo

---
+++ binanyExponentiation.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int convertToBinary(int number)
+{
+    // convert the number to binary
+    int binary = 0;
+    int power = 0;
+    while (number > 0)
+    {
+        int remainder = number % 2;
+        number = number / 2;
+        binary = (remainder * pow(10, power)) + binary;
+        power++;
+    }
+    return binary;
+}
+
+int main()
+{
+
+    // calculate 3^5
+    int base = 3;
+    int n = 5;
+
+    int binaryPower = convertToBinary(n);
+    cout << n;
+    int ans = 1;
+    int x = base;
+    while (binaryPower > 0)
+    {
+        int lastDigit = binaryPower % 10;
+        if (lastDigit == 1)
+        {
+            ans = ans * x;
+        }
+        x = x * x;
+        binaryPower = binaryPower / 10;
+    }
+
+    cout << "Answer : " << ans;
+
+    return 0;
+}
